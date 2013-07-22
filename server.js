1ed76f42c2f095e3b554e8cd747483cc075a23a0
James Pasko <jbpasko@gmail.com>
2013-07-22 11:50:15 -0700

    simple adspace server

--- server.js
+++ server.js
@@ -6,19 +6,29 @@ if (!process.env.NODE_ENV) {
 var db = new AWS.DynamoDB();
 var express = require('express');
 var app = express();
+var panam = true;
 
 app.get('/', function(request, response) {
     response.send('Hello World');
 });
 
 app.get('/adspace/:id', function(request, response) {
-    response.send(
-	{
+    if (panam) {
+	response.send({
 	    "title": "Pan American",
 	    "text": "Overnight to Europe in your own private stateroom",
 	    "image": "https://s3.amazonaws.com/project-r/pan-am-logo.png",
 	    "link": "https://www.google.com"
 	});
+    } else {
+	response.send({
+	    "title": "Texaco",
+	    "text": "The Texas Company",
+	    "image": "https://s3.amazonaws.com/project-r/texaco-logo.jpg",
+	    "link": "https://www.google.com"
+	});
+    }
+    panam = !panam;
 });
 
 app.listen(process.env.PORT || 8888);
