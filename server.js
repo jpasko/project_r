bc39565edea7bd93ba0cc904fa30c63681057a16
James Pasko <jbpasko@gmail.com>
2013-07-22 10:34:14 -0700

    aws-sdk configuration

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 var AWS = require('aws-sdk');
+if (!process.env.NODE_ENV) {
+    AWS.config.loadFromPath('./.local/credentials.json');
+}
+var db = new AWS.DynamoDB();
+db.listTables(function(err, data) {
+    console.log(data.TableNames);
+});
+
+var s3 = new AWS.S3();
+s3.listBuckets(function(err, data) {
+    for (var index in data.Buckets) {
+	var bucket = data.Buckets[index];
+	console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+    }
+});
+
 var http = require('http');
 var express = require('express');
 var app = express();
